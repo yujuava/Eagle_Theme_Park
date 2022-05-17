a21a88747ac3081c42f66f23433a3fb2b98caaa7
yujuava <yju031769@gmail.com>
2022-05-17 16:47:20 +0800

    gulpfile更新

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,119 @@
-const{src , dest , watch , series ,parallel} = require('gulp');
+//套件方法的引入
+const { src, dest, watch, series, parallel } = require('gulp');
+
+
+// 第一個任務 consloe log
+function defaultTask(cb) {
+  console.log('gulp 任務執行成功')
+  cb();
+}
+
+//exports.default = defaultTask
+
+
+
+// a 任務
+function TaskA(cb) {
+  console.log('A任務')
+  cb();
+}
+// b 任務 
+function TaskB(cb) {
+  console.log('B任務')
+  cb();
+}
+
+// 非同步  
+exports.async = series(TaskA, TaskB);
+
+//同步
+exports.sync = parallel(TaskA, TaskB);
+
+
+
+// src -> dest  html
+// function move() {
+//   return src('./src/*.html')
+//   .pipe(dest('dist'))
+// }
+// exports.m = move
+
+// html package
+const fileinclude = require('gulp-file-include');
+
+function includeHTML() {
+    return src('src/*.html') // 來源
+        .pipe(fileinclude({
+            prefix: '@@',
+            basepath: '@file'
+        }))// fileinclude function
+        .pipe(dest('./dist'));// 目的地
+}
+
+exports.html =  includeHTML;
+
+
+
+
+
+// js move
+function moveJs() {
+  return src('src/js/*.js').pipe(dest('dist/js'))
+}
+
+//img move
+function moveImg() {
+  return src('src/images/*.*').pipe(dest('dist/images'))
+}
+
+
+
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+
+
+function styleSass() {
+  return src('./src/sass/*.scss')
+      .pipe(sourcemaps.init())
+      .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+      .pipe(sourcemaps.write())
+      .pipe(dest('./dist/css'));
+}
+
+
+
+
+// 監看
+function watchfile() {
+  watch(['src/*.html' , 'src/**/*.html'], includeHTML)    // 監看html
+  //  watch('js/*.js' , moveJs)  // 監看js
+  //  watch('css/*.css' , moveCss)  // 監看js
+  watch(['./src/sass/*.scss' ,'./src/sass/**/*.scss'], styleSass) // 監看sass
+}
+
+// 瀏覽器同步
+const browserSync = require('browser-sync');
+const reload = browserSync.reload;
+
+
+function browser(done) {
+    browserSync.init({
+        server: {
+            baseDir: "./dist",
+            index: "index.html"
+        },
+        port: 3000
+    });
+    watch(['src/*.html' , 'src/**/*.html'], includeHTML).on('change' , reload)    // 監看html
+    watch('src/js/*.js' , moveJs)  // 監看js
+    watch(['src/images/*.*', 'src/images/**/*.*'], moveImg)  // 監看 img
+    watch(['./src/sass/*.scss' ,'./src/sass/**/*.scss'], styleSass).on('change' , reload) // 監看sass
+    done();
+}
+
+
+// 監看
+exports.w =  series(parallel(moveJs,moveImg,includeHTML,styleSass), watchfile)  
+
+//瀏覽器同步
+exports.default =  series(parallel(moveJs,includeHTML,styleSass,moveImg), browser)  
