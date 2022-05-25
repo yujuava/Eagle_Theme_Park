7871b01fd9bda8b20f93f8aaccff006e80d39816
yujuava <yju031769@gmail.com>
2022-05-26 01:45:21 +0800

    更新news動態&購物車

--- src/js/MNevents.js
+++ src/js/MNevents.js
@@ -1,4 +1,4 @@
-(function () {
+(function mnchange() {
     const aaaWidth = document.querySelector('.aaa').offsetWidth //1536
     const fixedWidth = document.querySelector('main').offsetWidth
     const buttonWidth = document.querySelector('button').offsetWidth
@@ -17,4 +17,53 @@
     document.addEventListener('mouseup', () => {
         document.removeEventListener('mousemove', documentMove)
     });
+    let timerId;
+    window.onscroll=function(){
+    window.clearTimeout(timerId)    
+        let num=5; 
+        if (window.scrollY >= "1430"){
+            function $id(id){
+                    return document.getElementById(id)
+                }
+                let timerId;
+                countDown();
+                    timerId =window.setInterval(countDown,500)//計時器單位是毫秒
+                function countDown(){
+                    num--;
+                    if(num==4){
+                        document.querySelector('main').style.width = 45 + '%'
+                        document.querySelector('button').style.left = 45 +'%'
+                        document.querySelector('main').classList.add('ease')
+                        document.querySelector('button').classList.add('ease')
+
+                    }
+                    if(num==3){
+                        document.querySelector('main').style.width = 50 + '%'
+                        document.querySelector('button').style.left = 50 +'%'
+                    }
+                    if(num==2){
+                        document.querySelector('main').style.width = 55 + '%'
+                        document.querySelector('button').style.left = 55 +'%'
+                    }
+                    if(num==1){
+                        document.querySelector('main').style.width = 50 + '%'
+                        document.querySelector('button').style.left = 50 +'%'
+                    }
+                    if(num==0){
+                        document.querySelector('main').classList.remove('ease')
+                        document.querySelector('button').classList.remove('ease')
+                    }
+                    
+                }
+
+            }
+            
+    }
+
+    window.onresize=mnchange;  
 })()
+
+
+
+
+
