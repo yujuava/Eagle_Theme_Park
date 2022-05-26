04fcd7faafee5c09d68ae49e7a29d141a06e95db
LAPTOP-PATF7J21\Tibame_T14 <melody19961110@gmail.com>
2022-05-27 00:10:14 +0800

    登入的開開關關燈箱做好嚕><!!!

--- src/js/hbgAndLightbox.show.js
+++ src/js/hbgAndLightbox.show.js
@@ -17,22 +17,32 @@ let login = document.getElementById("login-lightbox");
 let closeBtn = document.querySelectorAll(".close");
 let registerBtn = document.getElementById("register-btn");
 let register = document.getElementById("register-lightbox");
-
+let alreadyhaveBtn = document.getElementById("alreadyhave-btn");
+// let fullScreen = document.querySelectorAll(".full-screen");
+let fullScreen = document.getElementById("full-screen");
 
 
 for(let i=0 ;i<memberBtn.length ; i++){
     memberBtn[i].addEventListener("click",function(){
-        login.classList.toggle("hide"); 
-        console.log(login);
+        fullScreen.classList.toggle("hide"); 
+        login.classList.remove("hide"); 
     })
     for(let j=0 ; j<closeBtn.length ;j++){
         closeBtn[j].addEventListener("click",function(){
             login.classList.add("hide"); 
+            register.classList.add("hide"); 
+            fullScreen.classList.add("hide"); 
         })
     }
     registerBtn.addEventListener("click",function(){
         login.classList.add("hide"); 
         register.classList.remove("hide"); 
+        // alert("hi");
+
+    })
+    alreadyhaveBtn.addEventListener("click",function(){
+        register.classList.add("hide"); 
+        login.classList.remove("hide"); 
 
     })
 
