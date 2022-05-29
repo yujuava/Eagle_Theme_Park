23fae41194bccffa2905df072ee5666fa8f6e9f1
nathanyeh <adonis10469@gmail.com>
2022-05-29 12:28:59 +0800

    food revised

--- src/js/food.js
+++ src/js/food.js
@@ -2,19 +2,21 @@
     let next = document.querySelectorAll(".Next");
     let back = document.querySelectorAll(".Back");
     let carousel = document.querySelectorAll(".carousel_cards");
-    let angle = 0;
+    let arr = [0, 0, 0];
 
     for(let i=0; i<next.length; i++){
         next[i].addEventListener("click",function(){
-             angle -= 45;
-             carousel[i].style.transform = `translateZ(-25rem) rotateY(${angle}deg)`; 
+             // angle
+             arr[i] -= 45;
+             carousel[i].style.transform = `translateZ(-25rem) rotateY(${arr[i]}deg)`; 
         });
     }
 
     for(let i=0; i<back.length; i++){
         back[i].addEventListener("click",function(){
-             angle += 45;
-             carousel[i].style.transform = `translateZ(-25rem) rotateY(${angle}deg)`; 
+             // angle
+             arr[i] += 45;
+             carousel[i].style.transform = `translateZ(-25rem) rotateY(${arr[i]}deg)`; 
         });
     }
 
