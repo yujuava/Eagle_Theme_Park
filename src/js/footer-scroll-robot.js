1ab5e1ad2512adf8f4fe86bdb92ca1e67e0a8b98
LAPTOP-PATF7J21\Tibame_T14 <melody19961110@gmail.com>
2022-05-31 17:49:57 +0800

    改變大票券路徑，客服機器人增加文字

--- src/js/footer-scroll-robot.js
+++ src/js/footer-scroll-robot.js
@@ -1,28 +1,28 @@
 // js 電腦版導覽列滾輪 機器人&top
 
-window.addEventListener("load",function(){  
-    document.addEventListener("scroll",function(){
-        let ScrollNum = document.documentElement.scrollTop;
-        let screenHeight = screen.height;
-        // console.log(screenHeight)
-        let robot = document.getElementById("robot");
-        let top = document.getElementById("top");
-        // console.log(ScrollNum)
-            if(ScrollNum>screenHeight){
-                robot.classList.remove("hide");
-                robot.classList.add("flex");
-                top.classList.remove("hide");
-                top.classList.add("flex");
-            }else{
-                robot.classList.remove("flex");
-                robot.classList.add("hide");
-                top.classList.remove("flex");
-                top.classList.add("hide");
-            }
+// window.addEventListener("load",function(){  
+//     document.addEventListener("scroll",function(){
+//         let ScrollNum = document.documentElement.scrollTop;
+//         let screenHeight = screen.height;
+//         // console.log(screenHeight)
+//         let robot = document.getElementById("robot");
+//         let top = document.getElementById("top");
+//         // console.log(ScrollNum)
+//             if(ScrollNum>screenHeight){
+//                 robot.classList.remove("hide");
+//                 robot.classList.add("flex");
+//                 top.classList.remove("hide");
+//                 top.classList.add("flex");
+//             }else{
+//                 robot.classList.remove("flex");
+//                 robot.classList.add("hide");
+//                 top.classList.remove("flex");
+//                 top.classList.add("hide");
+//             }
 
-    }, false);
+//     }, false);
 
-});
+// });
 
 // gsap footer氣球
 gsap.to(".ft-ballon1",{
