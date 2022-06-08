b4b9ab194c2e1016e39fecb1c8fe7a601d63478c
nathanyeh <adonis10469@gmail.com>
2022-06-09 01:06:06 +0800

    product list revised

--- src/productList.php
+++ src/productList.php
@@ -2,7 +2,7 @@
 // 於HTML結構前預先載入PHP並與資料庫進行連線
 // HMTL內容為測試資料 若能獲取商品資料代表正確執行
 try{
-	require_once("./connectLocalDb.php");
+	require_once("../mySQL/connectLocalDb.php");
 
 	$sql = "select * from product"; //準備好sql指令
 	$product = $pdo->query($sql); //將sql指令送到mysql去執行, 回傳的是pdoStatement
@@ -22,20 +22,36 @@ try{
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<style>
+	body{
+		background-color: #FFFBEF;
+	}
+	th {
+                background-color: #A7D4D3;
+                padding: 7px;
+        }
+
+    td {
+            vertical-align: middle;
+            padding: 10px;
+			text-align: center;
+			border-bottom: 1px solid #A7D4D3;
+        }
+</style>
 <body>
 
 
 <table align='center' class='productTable'>
-	<tr><th>商品編號</th><th>商品名稱</th><th>商品價格</th><th>商品資訊</th><th>商品圖片</th><th>商品狀態</th><th>建立日期</th></tr>
+	<tr><th>商品編號</th><th>商品圖片</th><th>商品名稱</th><th>商品價格</th><th>商品資訊</th><th>商品狀態</th><th>建立日期</th></tr>
 <?php 	
 	while($prodRow = $product->fetch(PDO::FETCH_ASSOC)){//取回一筆資料放到關聯性陣列中,
 ?>
 		<tr>
 		<td><?=$prodRow["product_no"]?></td>
+		<td><img width="100" src="images/<?=$prodRow["product_pic"]?>"></td>
 		<td><?=$prodRow["product_name"]?></td>
 		<td><?=$prodRow["product_price"]?></td>
 		<td><?=$prodRow["product_infor"]?></td>
-        <td><?=$prodRow["product_pic"]?></td>
         <td><?=$prodRow["product_st"]?></td>
         <td><?=$prodRow["product_creat"]?></td>
 		</tr>
