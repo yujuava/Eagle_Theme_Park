36bc6e370313c53be8ffba9c30c1b6459bed36c3
3shin44 <3shin44@gmail.com>
2022-06-13 03:02:14 +0800

    註冊功能

--- src/js/hbgAndLightbox.show.js
+++ src/js/hbgAndLightbox.show.js
@@ -3,7 +3,7 @@ let headerVue = new Vue({
     data: {     // 變數放這裡!
         mobileHbgShow: false,   //漢堡
         loginBoxShow:false, //登入燈箱
-        RegisterBoxShow:false,
+        RegisterBoxShow:true,
         //註冊燈箱
 
         //註冊燈箱顯示訊息
@@ -53,14 +53,21 @@ let headerVue = new Vue({
         },
         validatePsw1(){
             if (this.inputPsw1.length<8){
-                this.pswResult1 = "PSW密碼長度不足";
+                this.pswResult1 = "密碼長度不足";
             }else{
-                this.pswResult1 = "等待密碼確認";
+                let passwordCheck = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/;
+                if(passwordCheck.test(this.inputPsw1)!=true){
+                    this.pswResult1 = "密碼請輸入大小寫英文及數字8~12碼";
+                }else{
+                    this.pswResult1 = "此密碼可用";
+                }
+                
+                
             }
         },
         validatePsw2(){
             if (this.inputPsw2.length<8){
-                this.pswResult2 = "確認PSW密碼長度不足";
+                this.pswResult2 = "密碼長度不足";
             }else{
                 if(this.inputPsw1 == this.inputPsw2){
                     this.pswResult1 = "密碼相符";
@@ -96,51 +103,78 @@ let headerVue = new Vue({
                 document.getElementById('password').disabled = false;
                 document.getElementById('checkPassword').disabled = false;
                 document.getElementById('register').disabled = true;
+                document.getElementById('register').addEventListener("click", ()=>{
+                    
+                    
+                            // 1. 宣告XMLHttpRequest()
+                    let xhr = new XMLHttpRequest();
+                    
+                    // 2.決定傳送方法POST, 傳送目標, true代表非同步執行
+                    xhr.open("POST", "./register_ajax.php", true);
+                    xhr.setRequestHeader("content-type","application/x-www-form-urlencoded");
+
+                    // 3. 封裝接收到的檔案  此處以JSON格式為例
+                    // 因為本案例不須跳轉頁面 在這邊把對應資料抓過來封裝成物件就好
+                    let dataset = {};
+                    dataset.memId = headerVue.inputAccount;
+                    dataset.memPsw = headerVue.inputPsw1;
+                    // 會員註冊時間
+                    let today = new Date();
+                    let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+                    dataset.memDate = date;
+            
+                    // 4. 透過JSON.stringify將處理好的物件封裝成JSON檔案
+                    let data_info = `json=${JSON.stringify(dataset)}`;
+
+                    // 5. XMLHttpRequest送出
+                    xhr.send(data_info);
+      
+                });
             }
         },
 
 
 
 
-        validate(){
-        let acount = document.getElementById('account').value;
-        let password = document.getElementById('password').value;
-        let checkPassword = document.getElementById('checkPassword').value;
-        let accept = document.getElementById('accept').checked;
+        // validate(){
+        // let acount = document.getElementById('account').value;
+        // let password = document.getElementById('password').value;
+        // let checkPassword = document.getElementById('checkPassword').value;
+        // let accept = document.getElementById('accept').checked;
 
-        // =================註冊驗證==================
-        //email(帳號)的正規表達式
-        // let userEmail = /^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z]+$/; 
+        // // =================註冊驗證==================
+        // //email(帳號)的正規表達式
+        // // let userEmail = /^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z]+$/; 
 
-        //密碼的正規表達式
-        let passwordCheck = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/;''
-        // alert(userEmail.test(acount))
+        // //密碼的正規表達式
+        // let passwordCheck = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/;''
+        // // alert(userEmail.test(acount))
 
-        // if(userEmail.test(acount)!=true){
-        //     alert("帳號請輸入e-mail格式");
+        // // if(userEmail.test(acount)!=true){
+        // //     alert("帳號請輸入e-mail格式");
+        // //     return;
+        // // }
+        // if(passwordCheck.test(password)!=true){
+        //     alert("密碼請輸入大小寫英文及數字8~12碼")
         //     return;
         // }
-        if(passwordCheck.test(password)!=true){
-            alert("密碼請輸入大小寫英文及數字8~12碼")
-            return;
-        }
-        if(password!=checkPassword){
-            alert("密碼確認不一致")
-            return;
-        }
-        if(accept!=true){
-            alert("請勾選是否同意會員條款")
-            return;
-        }
-        if(acount==''||password==''||passwordCheck==''){
-            alert("還有欄位未輸入唷")
-            return;
-        }
-        if(userEmail.test(acount)==passwordCheck.test(password)==(password==checkPassword==true)==accept && ((acount!='')&&(password!='')&&(passwordCheck!=''))){
-            alert('恭喜註冊成功')
-            return;
-        }
-        },
+        // if(password!=checkPassword){
+        //     alert("密碼確認不一致")
+        //     return;
+        // }
+        // if(accept!=true){
+        //     alert("請勾選是否同意會員條款")
+        //     return;
+        // }
+        // if(acount==''||password==''||passwordCheck==''){
+        //     alert("還有欄位未輸入唷")
+        //     return;
+        // }
+        // if(userEmail.test(acount)==passwordCheck.test(password)==(password==checkPassword==true)==accept && ((acount!='')&&(password!='')&&(passwordCheck!=''))){
+        //     alert('恭喜註冊成功')
+        //     return;
+        // }
+        // },
         //拿到localstorage裡carts
         getCarts() {
             if (localStorage.getItem('carts'))
--- src/php/register_ajax.php
+++ src/php/register_ajax.php
@@ -10,12 +10,15 @@ try{
 
 	// 3. 準備寫入資料庫指令, 
 	// insert這種落落長的指令 不用特別指定欄位(mem_id=:mem_id (X) ), 直接擺value上去就好
-	$sql = "INSERT INTO `member` (`mem_no`, `mem_name`, `mem_lastname`, `mem_id`, `mem_psw`, `mem_tel`, `mem_mail`, `mem_state`, `mem_address`, `mem_country`, `mem_birth`, `mem_date`) VALUES (NULL, `mem_name`, `mem_lastname`, :mem_id, :mem_psw, `mem_tel`, `mem_mail`, 0, `mem_address`, `mem_country`, '2022-06-01', '2022-06-01')";
+	$sql = "INSERT INTO `member` (`mem_no`, `mem_id`, `mem_psw`, `mem_state`, `mem_date`) VALUES (NULL, :mem_id, :mem_psw, 0, :mem_date)";
+
+	// $sql = "INSERT INTO `member` (`mem_no`, `mem_name`, `mem_lastname`, `mem_id`, `mem_psw`, `mem_tel`, `mem_mail`, `mem_state`, `mem_address`, `mem_country`, `mem_birth`, `mem_date`) VALUES (NULL, `mem_name`, `mem_lastname`, :mem_id, :mem_psw, `mem_tel`, `mem_mail`, 0, `mem_address`, `mem_country`, '2022-06-01', '2022-06-01')";
 	
 	// 4. 合成SQL指令, 並將綁定對應值
 	$register = $pdo->prepare($sql);
 	$register->bindValue(":mem_id", $dataset["memId"]);
 	$register->bindValue(":mem_psw", $dataset["memPsw"]);
+	$register->bindValue(":mem_date", $dataset["memDate"]);
 	$register->execute();
 
 }catch(PDOException $e){
