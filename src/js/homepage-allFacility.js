ceb813a9e3c2cfc82bd219da62333972aa9d0c74
LAPTOP-PATF7J21\Tibame_T14 <melody19961110@gmail.com>
2022-06-13 16:38:58 +0800

    好

--- src/js/homepage-allFacility.js
+++ src/js/homepage-allFacility.js
@@ -3,80 +3,35 @@ Vue.component('card-component',{
     <div class="card card-bdr">
         <div class="pic card-bdr">
             <div class="area">
-                {{facilityRow.fac_area}}<br>
-                {{facilityRow.fac_area}}
+            {{item.fac_area}}<br>
+               {{item.fac_area}}
             </div>
         </div>
         <div class="info">
-            <h5>{{facilityRow.fac_name}}</h5>
-            <p>{{facilityRow.fac_descrip}}</p>
+            <h5>{{item.fac_name}}</h5>
+            <p>{{item.fac_descrip}}</p>
         </div>
     </div>
 
     `,
     props:{
-        item:Object
+        item:Object,
     }
 });
 
 new Vue({
     el:'#all-thrill-ride',
     data:{
-        currentFilter: 'ALL',   //自己設定 currentFilter的初始屬性值為ALL
+        item:[],
+        facilityRows:[],
     },
     // new
-    data() {
-        return {
-            facilityRows:[],
-        }
-    },
     computed: {  // 函數也可以放這裡，但是放在這裡的函數不能傳參數，一定要有傳回值(return)
-
-        //回傳名字
-        // cardTitles(){
-        //     this.card.map(function(v){
-        //         return v.name;
-        //     })
-        // },
-
-
-        //第一個篩選
-        // cardsFilter1(){
-        //     if(this.currentFilter == "ALL")return this.facilityRows;
-        //     return this.facilityRows.filter(v => v.fac_area == this.currentFilter)
-        // },
-
-        //第貳個篩選
-        // cardsFilter2() {
-        //     return this.cardsFilter1.filter(item => {       //return第一次篩選完的結果並且再篩一次
-        //         let rainPass = true;    //先宣告雨天 | 孕婦 | 輪椅 =  true
-        //         let pregnantPass = true;
-        //         let wheelchairPass = true;
-        //         if (this.rain){rainPass = item.rain};   //如果this.rain = true(就是物件裡有自選成true的那些設施)，
-        //         if (this.pregnant) pregnantPass = item.pregnant;    //
-        //         if (this.wheelchair) wheelchairPass = item.wheelchair;
-        //         // console.log(rainPass)
-        //         return rainPass && pregnantPass &&wheelchairPass;
-        //     });
-        // },
-        facilityRow(){
-            let results =[]
-            this.facilityRows.foreach( v => {
-                results.push({
-                    key: v.fac_no,
-                    name: str,
-                })
-            })
-            alert(v)
-            return results;
-        },
     },
     methods: {  // 函數大部分放這裡!
-
-        setFilter(filter) {
-            this.currentFilter = filter;
-		},   
-
+        // setFilter(filter) {
+        //     this.currentFilter = filter;
+		// },   
     },
     //new
     mounted(){
