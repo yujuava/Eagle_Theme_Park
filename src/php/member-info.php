1ceff0f0d37166a425ecc495a1188d2ddea1b9da
brady293 <brady0330y@gmail.com>
2022-06-13 23:13:36 +0800

    帳號又壞掉拉

--- src/php/member-info.php
+++ src/php/member-info.php
@@ -10,11 +10,12 @@ try{
     require_once("../connect_cgd101g3.php");
 
     //寫入資料庫指令
-    $sql = "update member set mem_name = :mem_name, mem_lastname = :mem_lastname, mem_id = 123, mem_psw = :mem_psw, mem_tel = :mem_tel, mem_mail = :mem_mail,mem_address = :mem_adress, mem_country = :mem_country where member.mem_no = :mem_no";
+    $sql = "update member set mem_name = :mem_name, mem_lastname = :mem_lastname, mem_id = :mem_id, mem_psw = :mem_psw, mem_tel = :mem_tel, mem_mail = :mem_mail,mem_address = :mem_adress, mem_country = :mem_country where member.mem_no = :mem_no";
 
 
     //合成sql指令,並綁訂對應值
     $members = $pdo->prepare($sql);
+    $members->bindValue(":mem_id",$_SESSION["mem_id"]);
     $members->bindValue(":mem_psw",$dataset["mem_psw"]);
     $members->bindValue(":mem_name",$dataset["mem_name"]);
     $members->bindValue(":mem_lastname",$dataset["mem_lastname"]);
