f8ddce874003aad57d755a78081b244204f589c6
LAPTOP-PATF7J21\Tibame_T14 <melody19961110@gmail.com>
2022-06-13 14:42:29 +0800

    先備份一下

--- src/js/back_discuss.js
+++ src/js/back_discuss.js
@@ -2,3 +2,31 @@
 function showAlert() {
     window.confirm("是否確認刪除?");
 }
+
+
+new Vue({
+    el:'#back',
+    data() {
+        return {
+            isOpen: false,
+            discussTitle: ['文章編號','成立時間','會員編號','標題內容'],
+            discussRows:[],
+        }
+    },
+    methods: {
+        // 燈箱   
+        show(){
+            this.isOpen = true;
+        },
+             
+    },
+    mounted(){
+        let xhr = new XMLHttpRequest();
+        xhr.onload = () => {
+            this.discussRows = JSON.parse(xhr.responseText);
+            console.log(this.discussRows);
+        }
+        xhr.open("get","./php/get_back_discuss.php",true);
+        xhr.send(null);
+    },
+})
