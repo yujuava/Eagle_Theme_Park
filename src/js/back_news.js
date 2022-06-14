6e3febe607f58639a9a72fab820e927bfdfc8b41
Robin926 <robin19962685@gmail.com>
2022-06-14 16:00:26 +0800

    no message

--- src/js/back_news.js
+++ src/js/back_news.js
@@ -1,14 +1,29 @@
 
-new Vue({
+let newsVue = new Vue({
     el:'#back',
-    data() {
-        return {
-            isOpen: false,
-        }
+    data:{
+        isOpen: false,
+        newsTitle: ['消息編號','消息名稱','消息內容','發布日期'],
+        newsRows:[],      //資料的陣列
     },
     methods: {   
         show(){
             this.isOpen = true;
         },     
+    },
+    mounted(){     //第一步仔入完成才開始執行            
+        console.log("init");         
+        let xhr = new XMLHttpRequest();    //建立ajax物件取XHR
+        // xhr.onload = function(){
+        //     this.memRows = JSON.parse(xhr.responseText);
+        //     alert(this.memRows);
+        //     console.log(this.memRows);
+        // }
+        xhr.onload = () => {   //等待回復訊息
+            newsVue.newsRows = JSON.parse(xhr.responseText); //收到打開資料,儲存到newVue的陣列
+            console.log(newsVue.newsRows);
+        }
+        xhr.open("get","php/back_news.php",true);   //決定請求送至PHP
+        xhr.send(null);                              //完成送出
     }
 })
