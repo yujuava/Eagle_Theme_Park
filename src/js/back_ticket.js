7436c9472b5b7f08badcdf2a1ce5c335ba3a1272
Robin926 <robin19962685@gmail.com>
2022-06-14 21:58:04 +0800

    票券資料

--- src/js/back_ticket.js
+++ src/js/back_ticket.js
@@ -1,14 +1,29 @@
 
-new Vue({
+let ticketVue = new Vue({
     el:'#back',
-    data() {
-        return {
-            isOpen: false,
-        }
+    data:{
+        isOpen: false,
+        ticketTitle: ['票券種類名稱','票券價格'],
+        ticketRows:[],      //資料的陣列
     },
     methods: {   
         show(){
             this.isOpen = true;
         },     
+    },
+    mounted(){     //第一步仔入完成才開始執行            
+        console.log("init");         
+        let xhr = new XMLHttpRequest();    //建立ajax物件取XHR
+        // xhr.onload = function(){
+        //     this.memRows = JSON.parse(xhr.responseText);
+        //     alert(this.memRows);
+        //     console.log(this.memRows);
+        // }
+        xhr.onload = () => {   //等待回復訊息
+            ticketVue.ticketRows = JSON.parse(xhr.responseText); //收到打開資料,儲存到newVue的陣列
+            console.log(newsVue.newsRows);
+        }
+        xhr.open("get","php/back_ticket.php",true);   //決定請求送至PHP
+        xhr.send(null);                              //完成送出
     }
 })
