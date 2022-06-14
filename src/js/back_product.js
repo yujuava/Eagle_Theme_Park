95ec1dbb2df31bfdb28b126525591ebb5f0d97d4
nathanyeh <adonis10469@gmail.com>
2022-06-14 15:46:18 +0800

    後台商品繼續努力

--- src/js/back_product.js
+++ src/js/back_product.js
@@ -12,8 +12,8 @@ var prods = new Vue({
             product_price: '',
             product_pic: ''
         },
-        objResult: {},//原來的物件
-        defaultResult: {},//新的更新過的資料
+        objResult: {},  // 原來的物件
+        defaultResult: {},  // 新的更新過的資料
     },
     computed: {
         currentItem() {
@@ -30,22 +30,36 @@ var prods = new Vue({
             this.currentNo = no;
             this.isOpen = true;
         },
-        addPro() {},
-        addHandler() {
+        addHandler() {  // 左上新增商品
             this.popup = JSON.parse(JSON.stringify(this.default));
             this.isOpen = true;
         },
         showAlert() {
             window.confirm("是否確認刪除?");
         },
-        async changeHandler() {//非同步//綁最後的按鍵
+        async changeHandler() {  //修改商品  // 非同步  // 綁最後的按鍵
             console.log('changeHandler')
-            let sendObj = JSON.stringify(this.popup);//取最後要再資料庫呈現的東西
+            let sendObj = JSON.stringify(this.popup);  // 取最後要再資料庫呈現的東西
             let xhr = new XMLHttpRequest();
             // 決定傳送方法POST, 傳送目標, true代表非同步執行
             xhr.open("POST","./php/update_back_product.php",true);
             xhr.setRequestHeader("content-type","application/x-www-form-urlencoded");
             xhr.send(`json=${sendObj}`);
+
+            window.confirm("是否確認修改?");
+            this.isOpen = false;
+        },
+        async addPro() {  //新增商品  // 非同步  // 綁最後的按鍵
+            console.log('addPro')
+            let sendObj = JSON.stringify(this.popup);  // 取最後要再資料庫呈現的東西
+            let xhr = new XMLHttpRequest();
+            // 決定傳送方法POST, 傳送目標, true代表非同步執行
+            xhr.open("POST","./php/update_back_product.php",true);
+            xhr.setRequestHeader("content-type","application/x-www-form-urlencoded");
+            xhr.send(`json=${sendObj}`);
+
+            window.confirm("是否確認新增?");
+            this.isOpen = false;
         },
     }
 })
