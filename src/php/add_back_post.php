97db546d82cc1921ff7b0d437c6f2369ecadc69f
brady293 <brady0330y@gmail.com>
2022-06-16 19:56:46 +0800

    新增還沒完成

---
+++ src/php/add_back_post.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+
+try{
+    require_once("../connect_cgd101g3.php");
+    $fileInfoArr = pathinfo($_FILES["product_pic"]["name"]);
+	$fileName = uniqid().".{$fileInfoArr["extension"]}"; 
+
+	$from = $_FILES["article_image"]["tmp_name"];
+	$to = "../images/$fileName";
+    if(copy( $from, $to)===true){
+        //寫入資料庫指令
+        $sql = "insert into `article` (`article_no`, `mem_no`, `article_title`, `article_date`, `article_content`, `article_image`) values (:article_title, :article_content, :article_image)";
+
+
+        //合成sql指令,並綁訂對應值
+        $post = $pdo->prepare($sql);
+        $post->bindValue(":article_title",$_POST["article_title"]);
+        $post->bindValue(":article_content",$_POST["article_content"]);
+        $post->bindValue(":article_image",$fileName);
+        $post->execute();
+        echo "異動成功~~";  
+    }
+    
+
+}catch(PDOException $e){
+	echo "系統暫時無法提供服務, 請聯絡系統維護人員<br>";
+	echo "錯誤訊息 : ", $e->getMessage(), "<br>";
+	echo "錯誤行號 : ", $e->getLine(), "<br>";
+}
+
+?>
+
