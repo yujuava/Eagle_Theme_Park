7760fd44fc2b95a27c726c8811864dc46ea9de6b
yujuava <yju031769@gmail.com>
2022-06-18 17:17:47 +0800

    票券訂單管理後台資料庫匯入ok

--- src/js/back_order_ticket.js
+++ src/js/back_order_ticket.js
@@ -1,14 +1,95 @@
+Vue.component('table-component',{
+    template:`
+    <table class="innerData" id="innerOrder">
+        <tr>
+            <th>票券名稱</th>
+            <th>購買單價</th>
+            <th>購買張數</th>
+        </tr>
+        <tr v-for="(item,index) in list" :key="index">
+            <td>{{item.ticket_name}}</td>
+            <td>{{item.ticket_price}}</td>
+            <td>{{item.ticket_total}}張</td>
+        </tr>
+    </table>
+    `,
+    props:{
+        detail:Object,
+    },
+    computed: {
+        list() {
+            return this.detail?.data ?? [];
+        },
+    },
+})
 
-new Vue({
+let orderVue = new Vue({
     el:'#back',
-    data() {
-        return {
-            isOpen: false,
-        }
+    data:{
+        isOpen: false,
+        orderRows:[],      //資料的陣列
+        results:[],
+        newObj:[],
+        orderlist:Object,
+        focusKey: '',
     },
     methods: {   
-        show(){
+        idpopup(ticket_order_no){
             this.isOpen = true;
-        },     
+            this.focusKey = ticket_order_no;
+        }
+    },
+    computed: {
+        showPopupItem() {
+            return this.newObj.find(v => v.ticket_order_no == this.focusKey) ?? this.newObj[0];
+        },
+        // showPopupItem() {
+        //     return this.newObj.ticket_order_no
+        // },
+    },
+    mounted(){     
+        
+        //第一步仔入完成才開始執行            
+        console.log("init");         
+        let xhr = new XMLHttpRequest();    
+        //建立ajax物件取XHR    
+        xhr.onload = () => {   //等待回復訊息
+            orderVue.orderRows = JSON.parse(xhr.responseText); 
+            //收到打開資料,儲存到newVue的陣列
+            let set = new Set();
+            orderVue.results = orderVue.orderRows.filter(item => !set.has(item.ticket_order_no) ? set.add(item.ticket_order_no) : false);
+            var map = {};
+            for(var i = 0; i < orderVue.orderRows.length; i++){
+                var eachObj = orderVue.orderRows[i];
+            if(!map[eachObj.ticket_order_no]){
+                    orderVue.newObj.push({
+                        ticket_order_no: eachObj.ticket_order_no,
+                        ticket_order_tp: eachObj.ticket_order_tp.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ","),
+                        ticket_order_time: eachObj.ticket_order_time,
+                        mem_name: eachObj.mem_name,
+                        mem_no: eachObj.mem_no,
+                        data: [eachObj]
+                    });
+                    map[eachObj.ticket_order_no] = eachObj;
+                }else{
+                    for(var j = 0; j < orderVue.newObj.length; j++){
+                        var dj = orderVue.newObj[j];
+                        if(dj.ticket_order_no == eachObj.ticket_order_no){
+                            dj.data.push(eachObj);
+                            break;
+                        }
+                    
+                    }
+                }
+            }
+            console.log("newObj:",orderVue.newObj);
+        }
+        
+        xhr.open("get","php/get_back_ticket_order.php",true);   
+        //決定請求送至PHP
+        xhr.send(null);                              
+        //完成送出
+        
+      
     }
 })
---
+++ src/php/get_back_ticket_order.php
@@ -0,0 +1,22 @@
+<?php
+// by hana
+session_start(); //
+try{
+  require_once("../connect_cgd101g3.php");
+  $sql = " SELECT t.ticket_name,t.ticket_price,tt.ticket_total,tor.mem_no,tor.ticket_order_tp,tor.ticket_order_shipping,tor.ticket_order_time,tor.ticket_order_no from ticket t join ticket_item tt on t.ticket_no = tt.ticket_no left join ticket_order tor on tt.ticket_order_no = tor.ticket_order_no where ticket_total > 0;";
+
+  $memberTicket = $pdo->prepare($sql);
+  $memberTicket ->execute();
+  $memberTicketRows = $memberTicket->fetchAll(PDO::FETCH_ASSOC);
+  echo json_encode($memberTicketRows);
+//  echo var_dump($memberOrderResult);
+
+}catch(PDOException $e){
+    echo"錯誤訊息:" ,$e->getMessage(),"<br>";
+    echo"錯誤行號:" ,$e->getLine(),"<br>";
+}
+?>
+
+
+
+
